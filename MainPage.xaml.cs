8d91db6149724613e6c20cb551610e7365964a66
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-09-01 21:21:15 +0200

    minor adjustments

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,14 +4,12 @@ namespace SpinnerPopup;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnBtnClicked(object sender, EventArgs e)
 	{
 		var popup = new SpinnerPopup();
 		this.ShowPopup(popup);
